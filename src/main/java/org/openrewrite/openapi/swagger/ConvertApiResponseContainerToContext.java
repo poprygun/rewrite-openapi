5616a65534b44dccdde60269d8d831369398d7cd
Alex Shumilov <ashumilov@vmware.com>
2024-02-26 15:54:20 -0500

    Set new annoation attribute value.

--- src/main/java/org/openrewrite/openapi/swagger/ConvertApiResponseContainerToContext.java
+++ src/main/java/org/openrewrite/openapi/swagger/ConvertApiResponseContainerToContext.java
@@ -20,18 +20,17 @@ import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.internal.ListUtils;
-import org.openrewrite.java.AnnotationMatcher;
-import org.openrewrite.java.JavaIsoVisitor;
-import org.openrewrite.java.JavaTemplate;
-import org.openrewrite.java.TreeVisitingPrinter;
+import org.openrewrite.java.*;
 import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaType;
 import org.openrewrite.java.tree.TypeUtils;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ConvertApiResponseContainerToContext extends Recipe {
 
@@ -47,58 +46,97 @@ public class ConvertApiResponseContainerToContext extends Recipe {
         return "Convert API response container to context.";
     }
 
+    private String annotationType = "io.swagger.v3.oas.annotations.responses.ApiResponse";
+
+    private String attributeName = "context";
+    private String attributeValue = "changeme";
+    boolean addOnly = true;
+
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(new UsesType<>(annotationType, false), new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public J.Annotation visitAnnotation(J.Annotation a, ExecutionContext ctx) {
+                if (!ANNOTATION_MATCHER.matches(a)) {
+                    return a;
+                }
 
-        return Preconditions.check(
-                new UsesType<>("io.swagger.v3.oas.annotations.responses.ApiResponse", true)
-                , new JavaIsoVisitor<ExecutionContext>() {
-                    @Override
-                    public J.Annotation visitAnnotation(J.Annotation a, ExecutionContext ctx) {
-                        if (ANNOTATION_MATCHER.matches(a)) {
-//                            return a.withArguments(ListUtils.map(a.getArguments(), this::maybeReplaceResponseCodeTypeAndValue));
-                            return JavaTemplate.builder("#{} = #{}")
+                List<Expression> currentArgs = a.getArguments();
+
+                boolean contextWasAlreadyAdded = currentArgs.stream().anyMatch(arg
+                        -> ((J.Identifier) ((J.Assignment) arg).getVariable()).getSimpleName().equalsIgnoreCase("context"));
+                if (contextWasAlreadyAdded) return a;
+
+                String newAttributeValue = maybeQuoteStringArgument(attributeName, attributeValue, a);
+
+                // First assume the value exists amongst the arguments and attempt to update it
+                AtomicBoolean foundAttributeWithDesiredValue = new AtomicBoolean(false);
+                final J.Annotation finalA = a;
+                List<Expression> newArgs = ListUtils.map(currentArgs, it -> {
+                    if (it instanceof J.Literal) {
+                        // The only way anything except an assignment can appear is if there's an implicit assignment to "value"
+                        if (attributeName == null || "value".equals(attributeName)) {
+                            if (newAttributeValue == null) {
+                                return null;
+                            }
+                            J.Literal value = (J.Literal) it;
+                            if (newAttributeValue.equals(value.getValueSource()) || Boolean.TRUE.equals(addOnly)) {
+                                foundAttributeWithDesiredValue.set(true);
+                                return it;
+                            }
+                            return ((J.Literal) it).withValue(newAttributeValue).withValueSource(newAttributeValue);
+                        } else {
+                            //noinspection ConstantConditions
+                            return ((J.Annotation) JavaTemplate.builder("value = #{}")
                                     .contextSensitive()
                                     .build()
-                                    .apply(getCursor(), a.getCoordinates().replaceArguments(), "context", "somecrazyvalue");
+                                    .apply(getCursor(), finalA.getCoordinates().replaceArguments(), it)
+                            ).getArguments().get(0);
                         }
-                        return a;
                     }
-
-                    private Expression maybeReplaceResponseCodeTypeAndValue(Expression arg) {
-                        if (arg instanceof J.Assignment) {
-                            J.Assignment assignment = (J.Assignment) arg;
-                            boolean matchesField = assignment.getVariable() instanceof J.Identifier &&
-                                    "response".equals(((J.Identifier) assignment.getVariable()).getSimpleName());
-
-                            if (matchesField){
-                                Expression target = ((J.FieldAccess) assignment.getAssignment()).getTarget();
-                            }
-                        }
-                        return arg;
-                    }
+                    return it;
+                });
+                if (foundAttributeWithDesiredValue.get() || newArgs != currentArgs) {
+                    return a.withArguments(newArgs);
                 }
-        );
+                // There was no existing value to update, so add a new value into the argument list
+                String effectiveName = (attributeName == null) ? "value" : attributeName;
+                //noinspection ConstantConditions
+                J.Assignment as = (J.Assignment) ((J.Annotation) JavaTemplate.builder("#{} = #{}")
+                        .contextSensitive()
+                        .build()
+                        .apply(getCursor(), a.getCoordinates().replaceArguments(), effectiveName, newAttributeValue)
+                ).getArguments().get(0);
+                List<Expression> newArguments = ListUtils.concat(as, a.getArguments());
+                a = a.withArguments(newArguments);
+                a = autoFormat(a, ctx);
+
+
+                return a;
+            }
+        });
     }
 
-    private static class ApiResponseAnnotationTransformer extends JavaIsoVisitor<ExecutionContext> {
-        private static final AnnotationMatcher API_RESPONSE_MATCHER = new AnnotationMatcher("@io.swagger.v3.oas.annotations.responses.ApiResponse(response = *, responseContainer = \"List\")");
-
-        @Override
-        public J.Annotation visitAnnotation(J.Annotation a, ExecutionContext executionContext) {
-//            if (API_RESPONSE_MATCHER.matches(a)) {
-//                List<Expression> currentArgs = a.getArguments();
-
-//                return JavaTemplate.builder("#{}")/*[Rewrite8 migration]`contextSensitive()` could be unnecessary and can be removed, please double-check manually*/.contextSensitive()
-//                        .build().apply(/*[Rewrite8 migration] please double-check correctness of this parameter manually, it could be updateCursor() if the value is updated somewhere*/getCursor(),
-//                                a.getCoordinates().replaceArguments(),
-//                                param1,
-//                                param2);
-
-//                return a;
-//            }
-            return super.visitAnnotation(a, executionContext);
+    @Nullable
+    private static String maybeQuoteStringArgument(@Nullable String attributeName, @Nullable String attributeValue, J.Annotation annotation) {
+        if ((attributeValue != null) && attributeIsString(attributeName, annotation)) {
+            return "\"" + attributeValue + "\"";
+        } else {
+            return attributeValue;
         }
+    }
+
+    private static boolean attributeIsString(@Nullable String attributeName, J.Annotation annotation) {
+        String actualAttributeName = (attributeName == null) ? "value" : attributeName;
+        JavaType.Class annotationType = (JavaType.Class) annotation.getType();
+        if (annotationType != null) {
+            for (JavaType.Method m : annotationType.getMethods()) {
+                if (m.getName().equals(actualAttributeName)) {
+                    return TypeUtils.isOfClassType(m.getReturnType(), "java.lang.String");
+                }
+            }
+        }
+        return false;
     }
 }
