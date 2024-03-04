a714c03c3fb843fabd78d021e99fd460503490b2
Alex Shumilov <ashumilov@vmware.com>
2024-03-04 14:17:19 -0500

    Add recipe to delete class if it uses specified package.

---
+++ src/main/java/org/openrewrite/openapi/swagger/RemoveClazz.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openrewrite.openapi.swagger;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.J;
+
+@Value
+@EqualsAndHashCode(callSuper=false)
+public class RemoveClazz extends Recipe{
+
+    @Option(displayName = "Java Type package",
+            description = "Class will be deleted when usage of types from this package found",
+            example = "com.yourorg")
+    String typePackage;
+
+    @Override
+    public String getDisplayName() {
+        return "Remove java class if it uses java type from specified package";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Remove java class if it uses java type from specified package.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public J.CompilationUnit visitCompilationUnit(J.CompilationUnit cu, ExecutionContext executionContext) {
+                if (cu.getImports().stream().anyMatch(anImport -> anImport.getPackageName().equals(typePackage))) {
+                    return null;
+                }
+                return super.visitCompilationUnit(cu, executionContext);
+            }
+        };
+    }
+}
